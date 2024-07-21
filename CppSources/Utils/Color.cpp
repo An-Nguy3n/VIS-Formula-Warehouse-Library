635aff2646a357a161d8be3f7966f75d01813097
An-Nguy3n <nguyenhuuan.k64.hust@gmail.com>
2024-07-20 18:39:07 -0700

    CppSources/Utils/

---
+++ CppSources/Utils/Color.cpp
@@ -0,0 +1,8 @@
+#pragma once
+#define RESET_COLOR     "\033[0m"
+#define FG_RED          "\033[1;31m"
+#define BG_RED          "\033[1;41m"
+#define FG_GREEN        "\033[1;32m"
+#define BG_GREEN        "\033[1;42m"
+#define FG_YELLOW       "\033[1;33m"
+#define BG_YELLOW       "\033[1;43m"
