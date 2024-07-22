c4c759b90c4cfc99b35791071976b83799022ab1
An-Nguy3n <nguyenhuuan.k64.hust@gmail.com>
2024-07-21 19:20:17 -0700

    set limit num_threads

--- HP_method_DYT_filter.cpp
+++ HP_method_DYT_filter.cpp
@@ -1,4 +1,7 @@
 #include "CppSources/Filter/DoubleYearThreshold/DoubleYearThresholdFilter.cpp"
+#include "omp.h"
+#include <iostream>
+using namespace std;
 
 
 int main(int argc, char *argv[]){
@@ -6,6 +9,9 @@ int main(int argc, char *argv[]){
         raise_error("Command chua nhap config_path", ".\\ExeFile\\HP_method_DYT_filter_CPU.exe <config_path>");
     }
 
+    int num_threads = omp_get_num_threads()-2;
+    cout << "Num threads: " << num_threads << endl;
+    omp_set_num_threads(num_threads);
     string config_path = argv[1];
     DoubleYearThresholdFilter vis(config_path);
     vis.run();
