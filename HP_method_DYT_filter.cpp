8148a525539b9af718b600f792c52a0e8208858d
An-Nguy3n <nguyenhuuan.k64.hust@gmail.com>
2024-07-21 19:11:11 -0700

    remove thread limit

--- HP_method_DYT_filter.cpp
+++ HP_method_DYT_filter.cpp
@@ -1,5 +1,4 @@
 #include "CppSources/Filter/DoubleYearThreshold/DoubleYearThresholdFilter.cpp"
-#include <omp.h>
 
 
 int main(int argc, char *argv[]){
@@ -7,7 +6,6 @@ int main(int argc, char *argv[]){
         raise_error("Command chua nhap config_path", ".\\ExeFile\\HP_method_DYT_filter_CPU.exe <config_path>");
     }
 
-    // omp_set_num_threads(8);
     string config_path = argv[1];
     DoubleYearThresholdFilter vis(config_path);
     vis.run();
