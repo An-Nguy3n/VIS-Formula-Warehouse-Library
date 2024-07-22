5cdeed5d9f500832d8d553bac5175fbb3add39ac
An-Nguy3n <nguyenhuuan.k64.hust@gmail.com>
2024-07-21 19:23:07 -0700

    set limit num_threads

--- HP_method_DYT_filter.cpp
+++ HP_method_DYT_filter.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[]){
         raise_error("Command chua nhap config_path", ".\\ExeFile\\HP_method_DYT_filter_CPU.exe <config_path>");
     }
 
-    int num_threads = omp_get_num_threads()-2;
+    int num_threads = omp_get_max_threads()-2;
     cout << "Num threads: " << num_threads << endl;
     omp_set_num_threads(num_threads);
     string config_path = argv[1];
